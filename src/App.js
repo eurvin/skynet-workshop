b740280646d1070d42581147b3104791c75b2243
Eurvin Morman <eurvin@gmail.com>
2021-10-05 20:55:13 +0200

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -41,14 +41,14 @@ const client = new SkynetClient();
  const seed = "verySecureSeed";
  const { publicKey, privateKey } = genKeyPairFromSeed(seed);
  const dataKey = "myResolverSkylinkForDocument";
- const skylink = "sia://XABvi7JtJbQSMAcDwnUnmp2FKDPjg8_tTTFP4BwMSxVdEg";
+ // const skylink = "sia://XABvi7JtJbQSMAcDwnUnmp2FKDPjg8_tTTFP4BwMSxVdEg";
 
  // set a registry entry to point at 'skylink'
- client.db.setDataLink(privateKey, dataKey, skylink);
+
 
  // get the resolver skylink which references the registry entry
- const resolverSkylink = getEntryLink(publicKey, dataKey)
-
+ const skylink = getEntryLink(publicKey, dataKey)
+  client.db.setDataLink(privateKey, dataKey, skylink);
 /************************************************/
 /*        Step 4.3 Code goes here               */
 /************************************************/
